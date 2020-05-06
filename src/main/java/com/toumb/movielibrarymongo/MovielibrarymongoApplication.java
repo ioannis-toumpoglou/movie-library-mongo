7e1fe2110a410b3158fe38f883c3ac5e8607992b
toumb <ioannis.toumboglou@gmail.com>
2020-05-06 22:25:01 +0300

    Initial commit

---
+++ src/main/java/com/toumb/movielibrarymongo/MovielibrarymongoApplication.java
@@ -0,0 +1,13 @@
+package com.toumb.movielibrarymongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovielibrarymongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovielibrarymongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/toumb/movielibrarymongo/MovielibrarymongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toumb.movielibrarymongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovielibrarymongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
